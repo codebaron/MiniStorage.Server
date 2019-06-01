7738b5af168fc71193ad5e6344a716e95db38ab4
Corey Baron <corey.baron@gmail.com>
2019-06-01 11:24:40 -0500

    create initial domain projects

---
+++ source/Domain.RentalInventory/Domain.RentalInventory.WebApi/Controllers/PropertyController.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="PropertyController.cs" company="Corey Baron">
+// Copyright (c) Corey Baron. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Domain.RentalInventory.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class PropertyController : Controller
+    {
+        //// GET: api/<controller>
+        ////[HttpGet]
+        ////public IEnumerable<string> Get()
+        ////{
+        ////    return new string[] { "value1", "value2" };
+        ////}
+
+        //// GET api/<controller>/5
+        ////[HttpGet("{id}")]
+        ////public string Get(int id)
+        ////{
+        ////    return "value";
+        ////}
+
+        //// POST api/<controller>
+        ////[HttpPost]
+        ////public void Post([FromBody]string value)
+        ////{
+        ////}
+
+        //// PUT api/<controller>/5
+        ////[HttpPut("{id}")]
+        ////public void Put(int id, [FromBody]string value)
+        ////{
+        ////}
+
+        //// DELETE api/<controller>/5
+        ////[HttpDelete("{id}")]
+        ////public void Delete(int id)
+        ////{
+        ////}
+    }
+}
