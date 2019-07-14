697d4dc569016d40fe33bcb1ba4c6867a7b5e208
Corey Baron <corey.baron@gmail.com>
2019-07-14 10:32:32 -0500

    initial database project created

---
+++ source/Domain.RentalInventory/Domain.RentalInventory.Database/Program.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Program.cs" company="Corey Baron">
+// Copyright (c) Corey Baron. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Domain.RentalInventory.Database
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
