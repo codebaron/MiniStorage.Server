6a3266fb39233a944f8f72f9d41b725c297d62ba
Corey Baron <corey.baron@gmail.com>
2019-07-19 12:36:42 -0500

    add initial system data for rental properties

---
+++ source/Domain.RentalInventory/Domain.RentalInventory.Database/Scripts/201907191227 - BaseSystemData.sql
@@ -0,0 +1,9 @@
+﻿USE [RentalInventory]
+GO
+
+INSERT INTO [dbo].[RentalProperty]([Id], [Name]) VALUES(1, 'Building A');
+INSERT INTO [dbo].[RentalProperty]([Id], [Name]) VALUES(2, 'Building B');
+INSERT INTO [dbo].[RentalProperty]([Id], [Name]) VALUES(3, 'Building C');
+INSERT INTO [dbo].[RentalProperty]([Id], [Name]) VALUES(4, 'Building D');
+INSERT INTO [dbo].[RentalProperty]([Id], [Name]) VALUES(5, 'Parking');
+GO
